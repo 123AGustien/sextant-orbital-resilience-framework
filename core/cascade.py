df437e49d65ce39abb0fa4ab5d04c63271de72dd
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-17 08:02:48 +0700

    Refactor failure propagation logic in CascadeEngine

--- core/cascade.py
+++ core/cascade.py
@@ -2,28 +2,45 @@ class CascadeEngine:
     def __init__(self, nodes, dependencies):
         self.nodes = set(nodes)
         self.dependencies = dependencies
-        self.failed = set()
 
-        # build reverse dependency map
         self.reverse_map = {}
         for d in dependencies:
             self.reverse_map.setdefault(d["to"], []).append(d["from"])
 
-    def fail_node(self, node):
-        if node in self.failed:
-            return
+    def step_propagate(self, current_failed):
+        """Compute next wave of failures"""
+        next_failed = set()
 
-        self.failed.add(node)
+        for node in current_failed:
+            for dependent in self.reverse_map.get(node, []):
+                next_failed.add(dependent)
 
-        # propagate failure
-        for dependent in self.reverse_map.get(node, []):
-            self.fail_node(dependent)
+        return next_failed
 
     def run(self, initial_failure):
-        self.fail_node(initial_failure)
+        timeline = []
+
+        # S1
+        current = {initial_failure}
+        all_failed = set()
+
+        step = 1
+
+        while current:
+            all_failed.update(current)
+
+            timeline.append({
+                "step": step,
+                "failed_this_step": list(current),
+                "total_failed": list(all_failed)
+            })
+
+            current = self.step_propagate(current) - all_failed
+            step += 1
 
         return {
             "initial_failure": initial_failure,
-            "failed_nodes": list(self.failed),
-            "healthy_nodes": list(self.nodes - self.failed)
+            "timeline": timeline,
+            "final_failed_nodes": list(all_failed),
+            "healthy_nodes": list(self.nodes - all_failed)
         }
