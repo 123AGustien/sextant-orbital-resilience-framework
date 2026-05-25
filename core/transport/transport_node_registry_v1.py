7a977bf9ea8aafea300debceafa9929dcf3b0d2a
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-26 02:31:33 +0700

    Define transport node types in transport_node_registry

---
+++ core/transport/transport_node_registry_v1.py
@@ -0,0 +1,52 @@
+"""
+🚆 Transport Node Registry v1
+Sextant Orbital Resilience Framework
+
+Defines valid transport system node types.
+"""
+
+
+TRANSPORT_NODE_TYPES = {
+
+    # ---------------------------------
+    # ROUTE SYSTEMS
+    # ---------------------------------
+
+    "ROUTE_CONTROLLER",
+    "TRAFFIC_ROUTER",
+    "LOGISTICS_ROUTE_NODE",
+
+    # ---------------------------------
+    # TRANSIT SYSTEMS
+    # ---------------------------------
+
+    "RAIL_NETWORK_NODE",
+    "ROAD_NETWORK_NODE",
+    "AIR_TRAFFIC_NODE",
+    "PORT_TERMINAL_NODE",
+
+    # ---------------------------------
+    # SCHEDULING SYSTEMS
+    # ---------------------------------
+
+    "SCHEDULE_COORDINATOR",
+    "TIMING_SYNCHRONIZER",
+    "DISPATCH_CONTROLLER",
+
+    # ---------------------------------
+    # LOGISTICS SYSTEMS
+    # ---------------------------------
+
+    "FREIGHT_COORDINATOR",
+    "CARGO_TRACKING_NODE",
+    "SUPPLY_CHAIN_GATEWAY",
+
+    # ---------------------------------
+    # MONITORING SYSTEMS
+    # ---------------------------------
+
+    "THROUGHPUT_MONITOR",
+    "CONGESTION_MONITOR",
+
+    # ---------------------------------
+    # REC
