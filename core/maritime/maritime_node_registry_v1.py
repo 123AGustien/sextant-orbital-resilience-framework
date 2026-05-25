884321c555bda8d3da0e72b4bfbf06f9f25b6d73
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-26 03:34:14 +0700

    Create maritime_node_registry_v1.py

---
+++ core/maritime/maritime_node_registry_v1.py
@@ -0,0 +1,30 @@
+"""
+🌊 Maritime Node Registry v1
+Sextant Orbital Resilience Framework
+
+Defines operational nodes for maritime systems.
+"""
+
+MARITIME_NODES = [
+
+    # ---------------------------------
+    # NAVIGATION SYSTEMS
+    # ---------------------------------
+    "NAVIGATION_CORE",
+    "GPS_RECEIVER",
+    "RADAR_ARRAY",
+
+    # ---------------------------------
+    # VESSEL CONTROL
+    # ---------------------------------
+    "ENGINE_CONTROL_SYSTEM",
+    "STEERING_MODULE",
+    "STABILITY_CONTROL",
+
+    # ---------------------------------
+    # LOGISTICS LAYER
+    # ---------------------------------
+    "CARGO_MANAGEMENT",
+    "PORT_INTERFACE",
+    "FLEET_SCHEDULER"
+]
