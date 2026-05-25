73b4c8e59e6f7ddeb5f3f00ac0fc9ce558f2396b
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-26 03:40:39 +0700

    Add cloud node registry definition
    Define a list of cloud nodes for the infrastructure.

---
+++ core/cloud/cloud_node_registry_v1.py
@@ -0,0 +1,31 @@
+"""
+☁️ Cloud Node Registry v1
+Sextant Orbital Resilience Framework
+
+Defines distributed compute and service infrastructure nodes.
+"""
+
+CLOUD_NODES = [
+
+    # ---------------------------------
+    # CORE COMPUTE
+    # ---------------------------------
+    "COMPUTE_CLUSTER_A",
+    "COMPUTE_CLUSTER_B",
+    "KUBERNETES_CONTROL_PLANE",
+    "MICROSERVICE_ORCHESTRATOR",
+
+    # ---------------------------------
+    # STORAGE LAYER
+    # ---------------------------------
+    "DISTRIBUTED_STORAGE_NODE",
+    "DATABASE_CLUSTER_PRIMARY",
+    "CACHE_LAYER_REDIS",
+
+    # ---------------------------------
+    # SERVICE LAYER
+    # ---------------------------------
+    "API_GATEWAY",
+    "AUTHENTICATION_SERVICE",
+    "MESSAGE_QUEUE_BROKER"
+]
