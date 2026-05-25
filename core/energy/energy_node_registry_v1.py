0aabaae8c5066aff2962baab438ed366f4f3ae37
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-05-26 03:37:36 +0700

    Create energy_node_registry_v1.py for energy nodes
    Defines a list of energy nodes for power generation, transmission, and distribution.

---
+++ core/energy/energy_node_registry_v1.py
@@ -0,0 +1,31 @@
+"""
+⚡ Energy Node Registry v1
+Sextant Orbital Resilience Framework
+
+Defines power grid and energy infrastructure nodes.
+"""
+
+ENERGY_NODES = [
+
+    # ---------------------------------
+    # GENERATION
+    # ---------------------------------
+    "POWER_PLANT_A",
+    "POWER_PLANT_B",
+    "SOLAR_FARM_GRID",
+    "WIND_TURBINE_CLUSTER",
+
+    # ---------------------------------
+    # TRANSMISSION
+    # ---------------------------------
+    "HIGH_VOLTAGE_GRID",
+    "SUBSTATION_ALPHA",
+    "SUBSTATION_BETA",
+
+    # ---------------------------------
+    # DISTRIBUTION
+    # ---------------------------------
+    "REGIONAL_DISTRIBUTION_NODE",
+    "SMART_METER_NETWORK",
+    "LOAD_BALANCER_CORE"
+]
