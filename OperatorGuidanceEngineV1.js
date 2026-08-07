5e92876b425ea3f4f4d2d7f82cf84ffcf3cfd9ec
Mr.Don Herman Oswald Weerasekera <donweerasekera@gmail.com>
2026-08-08 00:10:59 +0700

    Update OperatorGuidanceEngineV1.js

--- OperatorGuidanceEngineV1.js
+++ OperatorGuidanceEngineV1.js
@@ -185,24 +185,3 @@ const OperatorGuidanceEngineV1 = {
 
 
 
-// =================================
-// GLOBAL EXPORT
-// =================================
-
-window.OperatorGuidanceEngineV1 =
-    OperatorGuidanceEngineV1;
-
-
-
-console.log(
-
-"🛰️ OperatorGuidanceEngineV1 ONLINE",
-
-window.OperatorGuidanceEngineV1
-
-);
-
-
-
-})();
-window.OperatorGuidanceEngineV1 = OperatorGuidanceEngineV1;
